ed2388ffeab2c4170083f063bde7ad7690c0e45d
Zachary Klein <zak@silver-chalice.com>
2023-01-12 00:07:27 -0600

    Removed excess code

--- grails-app/init/mngrailsdemo/Application.groovy
+++ grails-app/init/mngrailsdemo/Application.groovy
@@ -5,14 +5,10 @@ import grails.boot.config.GrailsAutoConfiguration
 
 import groovy.transform.CompileStatic
 
-import javax.persistence.criteria.CriteriaBuilder
-import javax.persistence.criteria.Expression
 
 @CompileStatic
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
-
-        Expression
     }
 }
